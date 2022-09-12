2d14b5aff76429cf2fcc2c06015266db891d0833
x15 <x15@x15s-Mac-mini.lan>
2022-09-12 10:43:45 -0400

    c3 sql code primer upload

---
+++ demo_code/c3_SQL_code.sql
@@ -0,0 +1,55 @@
+
+/* Basic Syntax*/
+
+SELECT * FROM table
+
+/* Basic EDA aggregation analysis #1*/
+
+SELECT COUNT(*)
+FROM table
+
+/* Basic EDA aggregation analysis #2*/
+
+SELECT COUNT(*)
+FROM table
+WHERE conditions
+
+/* Basic EDA aggregation analysis #3*/
+
+SELECT DISTINCT(column)
+FROM table
+
+
+/* Basic SQL query #1*/
+
+SELECT *
+FROM table
+WHERE conditions AND conditions
+
+
+/* Basic SQL query #2*/
+
+UPDATE table
+SET (new column) = 'value'
+WHERE (old column)= 'value';
+UPDATE table
+SET (new column) = 'value'
+WHERE (old column)= 'value'
+
+
+
+WHERE clause conditions can contain the following operators:
+
+> Greater than. 
+< Less than. 
+= Equal. 
+!= Not equal. 
+>= Greater or equal. 
+<= Less than or equal. 
+IN 
+IS NULL 
+IS NOT NULL 
+
+
+
+
